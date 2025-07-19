0c022112f297603989b5c74bf1100908c4c1cb9c
Amel Junuzović <maniaba@outlook.com>
2025-07-19 14:44:47 +0200

    feat: add data manipulation methods to ArrayContext class

--- src/Context/ArrayContext.php
+++ src/Context/ArrayContext.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Maniaba\RuleEngine\Context;
 
-use Tests\Context\ArrayContextTest;
 
 /**
  * @see ArrayContextTest
@@ -24,4 +23,65 @@ final class ArrayContext implements ContextInterface
     {
         return \array_key_exists($field, $this->data);
     }
+
+    public function getData(): array
+    {
+        return $this->data;
+    }
+
+    public function setField(string|int|float $field, mixed $value): void
+    {
+        $this->data[$field] = $value;
+    }
+
+    public function removeField(string $field): void
+    {
+        unset($this->data[$field]);
+    }
+
+    public function clear(): void
+    {
+        $this->data = [];
+    }
+
+    public function __toString(): string
+    {
+        return json_encode($this->data, JSON_THROW_ON_ERROR);
+    }
+
+    public function toArray(): array
+    {
+        return $this->data;
+    }
+
+    public function merge(array $data): void
+    {
+        $this->data = array_merge($this->data, $data);
+    }
+
+    public function getKeys(): array
+    {
+        return array_keys($this->data);
+    }
+
+    public function getValues(): array
+    {
+        return array_values($this->data);
+    }
+
+    public function count(): int
+    {
+        return count($this->data);
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->data === [];
+    }
+
+    public function filter(callable $callback): array
+    {
+        return array_filter($this->data, $callback);
+    }
+
 }
